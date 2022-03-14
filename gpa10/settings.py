9119ff41c3adcfdbc2349049cd5f078d44426346
naominichols <98562952+naominichols@users.noreply.github.com>
2022-03-14 14:44:58 -0400

    Github Actions CI setup

--- gpa10/settings.py
+++ gpa10/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import sys
 from pathlib import Path
 import django_heroku
 
@@ -84,12 +84,21 @@ WSGI_APPLICATION = 'gpa10.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'test' in sys.argv:  # testing database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:  # regular database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
--- wordofmouth/tests.py
+++ wordofmouth/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class TrivialTests2(TestCase):
+    def test_trivial2(self):
+        self.assertEqual(1, 1)
