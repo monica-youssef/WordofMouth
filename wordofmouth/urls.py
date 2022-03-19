94221aa0a1674d72112ffff0deb69d768b4dafbe
alexbckr <amb3yn@virginia.edu>
2022-03-18 22:30:35 -0400

    add recipe list

--- wordofmouth/urls.py
+++ wordofmouth/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.homeview, name="homeview"),
     path('create_recipe', views.create_recipe, name='create_recipe'),
     path('detail', views.detail, name='detail'),
+    path('recipe_list', views.RecipeList.as_view(), name='recipe_list'),
 ]
