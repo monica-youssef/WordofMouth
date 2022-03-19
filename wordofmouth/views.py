8871765823dc9404e3c728bafc29fdee84423548
alexbckr <amb3yn@virginia.edu>
2022-03-18 22:30:35 -0400

    add recipe list

--- wordofmouth/views.py
+++ wordofmouth/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
+from django.views import generic
 
 from .models import Recipe
 
 def index(request):
-    return render(request, 'wordofmouth/index.html', {})
+    return render(request, 'index.html', {})
 
 
 def homeview(request):
@@ -14,7 +14,15 @@ def homeview(request):
 
 
 def detail(request):
-    return render(request, 'detail.html', {})
+    return render(request, 'wordofmouth/recipe_list.html', {})
+
+
+class RecipeList(generic.ListView):
+    template_name = 'wordofmouth/recipe_list.html'
+    context_object_name = 'recipe_list'
+
+    def get_queryset(self):
+        return Recipe.objects.all()
 
 
 def create_recipe(request):
@@ -24,9 +32,9 @@ def create_recipe(request):
         recipe.text = request.POST['text']
         recipe.id = Recipe.objects.all().count() + 1
     except (KeyError, recipe.DoesNotExist):
-        return render(request, 'detail.html', {
+        return render(request, 'wordofmouth/recipe_list.html', {
             'error_message': "You didn't enter a title and text."
         })
     else:
         recipe.save()
-        return HttpResponseRedirect('detail')
+        return HttpResponseRedirect('recipe_list')
