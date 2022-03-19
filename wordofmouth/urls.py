20596f787eb434b014d1509fb6de700f6ac0f925
alexbckr <amb3yn@virginia.edu>
2022-03-18 22:41:32 -0400

    make recipes clickable

--- wordofmouth/urls.py
+++ wordofmouth/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.homeview, name="homeview"),
     path('create_recipe', views.create_recipe, name='create_recipe'),
-    path('detail', views.detail, name='detail'),
+    path('recipe/<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('recipe_list', views.RecipeList.as_view(), name='recipe_list'),
 ]
