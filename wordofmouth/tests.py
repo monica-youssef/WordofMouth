6830218e31dff9184ebf0199f8f0c3e2877caff3
Willl99 <98544305+Willl99@users.noreply.github.com>
2022-03-23 18:36:33 -0400

    Created a Model test to make sure the title, text, and ID values are correct when a new object is created.

--- wordofmouth/tests.py
+++ wordofmouth/tests.py
@@ -1,8 +1,15 @@
 from django.test import TestCase
-
+from wordofmouth.models import Recipe
 # Create your tests here.
 
 
 class TrivialTests2(TestCase):
     def test_trivial2(self):
         self.assertEqual(1, 1)
+
+class recipeTestCase(TestCase):
+    def test_model_recipe_create(self):
+        recipe = Recipe.objects.create(title = "Gourmet Waffles", text = "Waffles")
+        self.assertEqual(recipe.id, 1)
+        self.assertEqual(recipe.title, "Gourmet Waffles")
+        self.assertEqual(recipe.text, "Waffles")
