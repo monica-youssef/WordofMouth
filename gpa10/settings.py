b48ef3bf49f292ae8d72fd660d8dc04b41b20e5c
naominichols <98562952+naominichols@users.noreply.github.com>
2022-03-23 16:05:56 -0400

    heroku issue, fixing allowed hosts

--- gpa10/settings.py
+++ gpa10/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = ***REMOVED***
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://group-project-a-10.herokuapp.com/',
+    '0.0.0.0',
+    '127.0.0.1',
+    '*',
+]
 
 
 # Application definition
