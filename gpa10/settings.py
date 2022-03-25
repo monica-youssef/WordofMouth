610efa598602f15a8abb84acba881e1af308d871
monica-youssef <may5u@virginia.edu>
2022-03-25 15:59:19 -0400

    more progress created on the like function

--- gpa10/settings.py
+++ gpa10/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
+    # for like feature
+    'posts',
 ]
 
 MIDDLEWARE = [
--- wordofmouth/admin.py
+++ wordofmouth/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recipe, Like
 
 # Register your models here.
+
+admin.site.register(Recipe)
+admin.site.register(Like)
