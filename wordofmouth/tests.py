c51a5d9166346654d93df5e77695c68ba4a4aa80
Willl99 <98544305+Willl99@users.noreply.github.com>
2022-03-25 18:38:44 -0400

    added three simple response tests

--- wordofmouth/tests.py
+++ wordofmouth/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
+from django.test.client import RequestFactory
 from wordofmouth.models import Recipe
+from . import views
 # Create your tests here.
+
 
 
 class TrivialTests2(TestCase):
@@ -13,3 +16,25 @@ class recipeTestCase(TestCase):
         self.assertEqual(recipe.id, 1)
         self.assertEqual(recipe.title, "Gourmet Waffles")
         self.assertEqual(recipe.text, "Waffles")
+
+class responseTests(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_response_index(self):
+        request = self.factory.get('wordofmouth/index/')
+        response = views.index(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_response_homeview(self):
+        request = self.factory.get('wordofmouth/')
+        response = views.homeview(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_response_detail(self):
+        request = self.factory.get('wordofmouth/recipe/1')
+        response = views.detail(request)
+        print(response)
+        self.assertEqual(response.status_code, 200)
+
+
