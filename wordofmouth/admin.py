b62f64b1678b65246ba5416068b0473d00b154d7
monica-youssef <may5u@virginia.edu>
2022-03-27 13:41:22 -0400

    reverted back to original

--- wordofmouth/admin.py
+++ wordofmouth/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Recipe, Like
 
 # Register your models here.
-
-admin.site.register(Recipe)
-admin.site.register(Like)
--- wordofmouth/models.py
+++ wordofmouth/models.py
@@ -1,35 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-
 
 # Create your models here.
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
     text = models.CharField(max_length=200)
     id = models.AutoField(primary_key=True)
-    liked = models.ManyToManyField(User, default=None, blank=True, related_name = 'liked')
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-    # everytime author is deleted the post they create is also deleted
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
 
     def __str__(self):
         return self.title
-
-    @property
-    def num_likes(self):
-        return self.liked.all().count()
-
-like_choices = (
-    ('Like', 'Like'),
-    ('Unlike', 'Unlike'),
-)
-
-class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True)
-    post = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    value = models.CharField(choices= like_choices, default='Like', max_length=10)
-
-    def __str__(self):
-        return str(self.post)
