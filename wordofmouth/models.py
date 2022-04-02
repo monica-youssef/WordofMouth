8687e33c8219866a30af015aa8639cbcf5d5bbbd
naominichols <98562952+naominichols@users.noreply.github.com>
2022-04-02 15:08:05 -0400

    added ingredients and instructions

--- wordofmouth/models.py
+++ wordofmouth/models.py
@@ -4,10 +4,17 @@ storage = GoogleCloudStorage()
 
 from django.contrib.auth.models import User
 # Create your models here.
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=200)
-    text = models.CharField(max_length=200)
-    added_by = models.ForeignKey(User ,null=True, blank=True, on_delete=models.SET_NULL)
+
+    # changed these for forking
+    ingredients = models.CharField(max_length=500)
+    instructions = models.CharField(max_length=500)
+    parent = models.CharField(max_length=500)
+
+    added_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
     id = models.AutoField(primary_key=True)
     image_url = models.CharField(max_length=200)
 
