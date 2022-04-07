1a846f9cf6acf9be5b8011b4a3785fd26c423066
Willl99 <98544305+Willl99@users.noreply.github.com>
2022-04-07 17:42:50 -0400

    Added ability to update the image associated with the recipe, although whether it works is uncertain since photo access is limited

--- wordofmouth/views.py
+++ wordofmouth/views.py
@@ -49,17 +49,6 @@ class RecipeList(generic.ListView):
     def get_queryset(self):
         return Recipe.objects.all()
 
-class EditView(generic.edit.UpdateView):
-    model = Recipe
-    fields = ['title', 'ingredients', 'instructions', 'image_url']
-    template_name_suffix = '_update_view'
-    def get_success_url(self):
-        return reverse('detail', kwargs={'pk': self.object.id})
-
-def edit_recipe_view(request):
-    return render(request, 'wordofmouth/recipe_update_view.html', {})
-
-
 def create_recipe(request):
     try:
         recipe = Recipe()
@@ -111,3 +100,13 @@ def LikeView(request, pk):
         liked = True
 
     return HttpResponseRedirect(reverse('detail', args=[str(pk)]))
+
+class EditView(generic.edit.UpdateView):
+    model = Recipe
+    fields = ['title', 'ingredients', 'instructions']
+    template_name_suffix = '_update_view'
+    def get_success_url(self):
+        return reverse('detail', kwargs={'pk': self.object.id})
+
+def edit_recipe_view(request):
+    return render(request, 'wordofmouth/recipe_update_view.html', {})
